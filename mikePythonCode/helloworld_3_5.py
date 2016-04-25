57d07bbef6738e7c903127407103a230a568c64c
Michael <MenfiSystems@Michaels-MacBook-Pro-2.local>
2016-04-25 02:17:45 -0500

    Updated the documentation dir, and the mikePythonCode dir

---
+++ mikePythonCode/helloworld_3_5.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3.5
+import sys 
+#
+print ("\nHello World!\n")
+#
+print ("sys.version is -> ")
+print(sys.version)
+print();
+#
+print ('sys.path is -> ')
+print(sys.path)
+print()
+#
+x = 5
+print ("x is ->", x)
+
+ 
+##Hello World!
+##
+##sys.version is -> 
+##3.5.1 (v3.5.1:37a07cee5969, Dec  5 2015, 21:12:44) 
+##[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)]
+##
+##sys.path is -> 
+##['/Users/MenfiSystems/Documents/machineLearning/python/pythonCode/udacityPythonCode2016', '/Users/MenfiSystems/Documents', '/Library/Frameworks/Python.framework/Versions/3.5/lib/python35.zip', '/Library/Frameworks/Python.framework/Versions/3.5/lib/python3.5', '/Library/Frameworks/Python.framework/Versions/3.5/lib/python3.5/plat-darwin', '/Library/Frameworks/Python.framework/Versions/3.5/lib/python3.5/lib-dynload', '/Library/Frameworks/Python.framework/Versions/3.5/lib/python3.5/site-packages']
+##
+##x is -> 5
+
+
+
+
+
+ 
