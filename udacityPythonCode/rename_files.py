53e9c0c00f9198dd3d6a5d79316664c080c4c0e3
Michael <MenfiSystems@Michaels-MacBook-Pro-2.local>
2016-05-02 04:37:14 -0500

    Update rename_files.py

--- udacityPythonCode/rename_files.py
+++ udacityPythonCode/rename_files.py
@@ -2,6 +2,7 @@
 
 #import os
 
+# import functions from the python os module
 from os import listdir 
 from os import getcwd
 from os import chdir
@@ -69,66 +70,5 @@ print ("\nBegin rename_files.py module")
 rename_files()
 
 print ("End rename_files.py module\n")
-
-
-# works
-#import os
-#os.listdir("foo")
-
-# works
-# from os import listdir
-# listdir("foo")
-
-
-
-
-
-
-
-
-##
-###
-### Python Standard Library
-###   file (module) name webbrowser   - function - def open
-###   file (module) name time         - function - def ctime ; def sleep
-###
-###
-### import time module
-##import time
-##
-### import webbrowser module
-##import webbrowser
-##
-##total_breaks = 2
-##break_count = 0
-##
-##print ("\nStarted break_time.py module")
-##
-##while (break_count < total_breaks):
-##    print ("\nBefore open Webbrowser" + time.ctime())
-##
-##    time.sleep(5)
-##    webbrowser.open("https://www.youtube.com/watch?v=PSKQ3ZNQ_O8")
-##    break_count = break_count + 1
-##
-##    print('break_count is -> {}'.format(break_count))
-##    print ("End open Webbrowser")
-##
-##
-##print ("\nEnded break_time.py module\n")
-###print()
-##
-##
-### print ("Before open Webbrowsr")
-##
-##
-### print ("End open Webbrowsr")
-##
-##
-##
- 
-
-
-
 
  
