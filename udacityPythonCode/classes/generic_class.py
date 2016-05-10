629fbf19d034e207f6fbe7eb326300787c63c4e6
Michael <MenfiSystems@Michaels-MacBook-Pro-2.local>
2016-05-10 12:15:15 -0500

    Update classes directory

---
+++ udacityPythonCode/classes/generic_class.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3.5
+
+class GenericClass():
+    pass
+
+
+myGenericClassInstance = GenericClass()
+
+
+# dir(myGenericClassInstance)
+print('\ndir(myGenericClassInstance) is -> {}\n'.format(dir(myGenericClassInstance)))
+
+# dir(myGenericClassInstance) is -> ['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__',
+# '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__le__', '__lt__', '__module__',
+# '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__',
+# '__subclasshook__', '__weakref__']
+
+
+
+
+
+
+ 
